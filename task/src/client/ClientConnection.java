4fa85c2b13c2e223c2dac7c9bc457d705e27f724
Dynotum <sedano.carlos93@gmail.com>
2021-01-24 10:07:34 -0600

    Using debbuger to pass this test

---
+++ task/src/client/ClientConnection.java
@@ -0,0 +1,46 @@
+package client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.List;
+
+class ClientConnection {
+
+    private final List<String> message;
+    private final String type;
+    private final int index;
+    private static int test = 1;
+
+    public ClientConnection(String type, int index, List<String> message) {
+        this.message = message;
+        this.type = type;
+        this.index = index;
+
+
+        System.out.println("Client started!");
+//        System.out.println(String.join(" ", type, String.valueOf(index), String.join(" ", message)));
+        clientConnect();
+    }
+
+    private void clientConnect() {
+        final String address = "127.0.0.1";
+        final int port = 55555;
+        try (Socket socket = new Socket(InetAddress.getByName(address), port)) {
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            final String responseServer = String.join(" ", type, String.valueOf(index), String.join(" ", message)).trim();
+
+            System.out.println("Sent: " + responseServer);
+            output.writeUTF(responseServer);
+
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            System.out.println("Received: " + input.readUTF());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- task/src/server/Main.java
+++ task/src/server/Main.java
@@ -13,8 +13,6 @@ public class Main {
     public static void main(String[] args) {
 
         final ConnectionServer server = new ConnectionServer();
-        //        JSONDatabase database = new JSONDatabase();
-//        database.start();
     }
 }
 
